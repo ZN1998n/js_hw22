266db5cbc5275f73be7fb191f475b72c768bcf00
ZN1998n <zafar.nigmatov1998@mail.ru>
2023-08-30 17:58:20 +0300

    first commit

--- script.js
+++ script.js
@@ -117,8 +117,78 @@ slideBtns.forEach(btn => {
         }
         slide.forEach(item => item.classList.add('hide'))
         slide[currentSlide].classList.remove('hide')
-        currentNum.textContent = `0${currentSlide+ 1}`
+        currentNum.textContent = `0${currentSlide + 1}`
     })
 })
+
+const timer = document.querySelectorAll('.timer__block')
+const days = document.querySelector('#days')
+const hours = document.querySelector('#hours')
+const minutes = document.querySelector('#minutes')
+const seconds = document.querySelector('#seconds')
+var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+var ctx = canvas.getContext('2d');
+
+
+function drawConfetti(x, y, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+
+function startConfetti() {
+  var colors = ['#ff0000', '#00ff00', '#0000ff'];
+
+
+  for (var i = 0; i < 100; i++) {
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var color = colors[Math.floor(Math.random() * colors.length)];
+
+    drawConfetti(x, y, color);
+  }
+}
+
+timer.forEach(time => {
+    const deadline = new Date('2023-08-30T19:00:00').getTime()
+
+    const interval = setInterval(() => {
+
+        const now = new Date().getTime()
+        const timeLeft = deadline - now
+
+        const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+        const hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        const minutesLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+        const secondsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000)
+
+
+        days.innerHTML = daysLeft
+        hours.innerHTML = hoursLeft
+        minutes.innerHTML = minutesLeft
+        seconds.innerHTML = secondsLeft
+
+        if (timeLeft <= 0) {
+            startConfetti()
+            clearInterval(interval)
+            
+        }
+
+
+    }, 1000)
+
+})
+
+
+
 
 
